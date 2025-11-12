07c6e88e7c2218836dd5c47a3387672837d1038d
Suhaib Farooqui <suhaib.farooqui@skylabs-ai.com>
2025-11-12 12:35:54 +0500

    hi

--- extraction.py
+++ extraction.py
@@ -2,5 +2,5 @@ from lean_dojo import LeanGitRepo, trace
 
 # Try this only if you are 100% sure the repo is public
 repo_url = "https://github.com/SuhaibFarooqui-123/test_extraction"
-repo = LeanGitRepo(repo_url, "f0fd9d8035378033d0eb0b8779d86bb4175f7f2b")
+repo = LeanGitRepo(repo_url, "a50b089d7c417dbe718ccf53d0ca786ea849e1c3")
 trace(repo, dst_dir="traced_test_extraction")
