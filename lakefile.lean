a50b089d7c417dbe718ccf53d0ca786ea849e1c3
Suhaib Farooqui <suhaib.farooqui@skylabs-ai.com>
2025-11-12 12:35:18 +0500

    Add lakefile

---
+++ lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package «test_extraction» {
+  -- add package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
