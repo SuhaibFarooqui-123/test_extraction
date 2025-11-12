050f22fe20343926edbffa3806e95c2697a21045
Suhaib Farooqui <suhaib.farooqui@skylabs-ai.com>
2025-11-12 12:13:13 +0500

    test

--- extraction.py
+++ extraction.py
@@ -1,4 +1,4 @@
 from lean_dojo import LeanGitRepo, trace
 
-repo = LeanGitRepo("https://github.com/yangky11/lean4-example", "7b6ecb9ad4829e4e73600a3329baeb3b5df8d23f")
+repo = LeanGitRepo("https://github.com/yangky11/lean4-example", "944e8bfe8848416ad1c2bdbfd519c79b5ae90668")
 trace(repo, dst_dir="traced_lean4-example")
